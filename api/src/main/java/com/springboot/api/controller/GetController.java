de0a1b25d5cf52712451ad3f0f12b29a4fbfef93
HueJo <slack_water@naver.com>
2024-07-04 11:06:30 +0900

    logback

--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,8 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -9,8 +11,12 @@ import java.util.Map;
 @RequestMapping("/api/v1/get-api")
 public class GetController {
 
+
+    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(GetController.class);
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다");
         return "Hello World (requestMapping)";
     }
 
